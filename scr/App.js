64c49ac2c994a12220bbe4ddf8a1b2f5f5a0ae6f
prasanth45bit <155311223+prasanth45bit@users.noreply.github.com>
2024-05-10 23:13:50 +0530

    Add files via upload

---
+++ scr/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Admin from './Admin';
+import ReactDOM from 'react-dom';
+import PieChart from './Chart';
+
+
+function App() {
+  return (
+    <Admin/>
+  );
+}
+
+export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
